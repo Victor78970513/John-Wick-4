cb031d4d9eb8af4f23a10f38b7171b0c2366c43a
Your Name <yer29.chok@gmail.com>
2023-06-11 18:46:36 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/movie" element={<MovieDetails />} />
+        <Route path="*" element={<p>NOT FOUND</p>}/>
       </Routes>
     </BrowserRouter>
   );
